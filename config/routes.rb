f0f050400d2e6e2674e39bbf87f0db76ad12c000
Pessi Virta <haamutili@gmail.com>
2019-02-19 16:55:25 +0100

    aasadajslkj

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/", to: "tasks#index", as: :list_tasks
+  # get "/tasks/new", to: "tasks#new", as: :new
+  # get "/tasks/:id", to: "tasks#show", as: :show
+  # post "/tasks", to: "tasks#create", as: :create
+  # get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
